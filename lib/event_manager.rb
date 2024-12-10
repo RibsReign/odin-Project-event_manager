191ee284a8b6204de95aadf5dd5cc1f5b14e0a40
Ondrej Hulík <hulik.ondrej@gmail.com>
2024-12-10 17:32:05 +0100

    Add peak hours and peak days

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -34,6 +34,41 @@ def legislators_by_zipcode(zipcode)
   end
 end
 
+def get_hour(regdate)
+  DateTime.strptime(regdate, '%m/%d/%y %H:%M').strftime('%k').to_i
+rescue ArgumentError
+  puts "Invalid date for row #{index}: #{regdate}"
+end
+
+def get_day(regdate)
+  DateTime.strptime(regdate, '%m/%d/%y %H:%M').wday
+rescue ArgumentError
+  puts "Invalid date for row #{index}: #{regdate}"
+end
+
+def calculate_peak_days(days)
+  weekday_names = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday]
+  days_occurrence = Array.new(6, 0)
+  days.each do |time|
+    days_occurrence[time] += 1
+  end
+  peak_days = days_occurrence.each_with_index.sort_by { |day, _idx| -day }.take(3).map { |_, idx| idx }
+  peak_days.map { |day| weekday_names[day] }
+end
+
+def calculate_peak_hours(hours)
+  hours_occurrence = Array.new(24, 0)
+  hours.each do |time|
+    hours_occurrence[time] += 1
+  end
+  hours_occurrence.each_with_index.sort_by { |hour, _idx| -hour }.take(3).map { |_, idx| idx }
+  # indexes = array
+  #        .each_with_index                # Pair numbers with indexes
+  #        .sort_by { |hours, _| -hours }  # Sort by the hours (descending)
+  #        .take(3)                        # Take the top three pairs
+  #        .map { |_, idx| idx }           # Extract the indexes
+end
+
 def save_thank_you_letter(id, form_letter)
   begin
     Dir.mkdir('output')
@@ -58,26 +93,25 @@ contents = CSV.open(
 # Open make template
 template_letter = File.read('form_letter.html')
 erb_template = ERB.new template_letter
-time = []
+hours = []
+days = []
 
 contents.each_with_index do |row, index|
   id = row[0]
   name = row[:first_name]
+  regdate = row[:regdate]
   zipcode = clean_zipcode(row[:zipcode])
   phone_number = clean_number(row[:homephone])
-  regdate = row[:regdate]
-  begin
-    time = DateTime.strptime(regdate, '%m/%d/%y %H:%M').strftime('%k:%M')
-    puts time
-  rescue ArgumentError
-    puts "Invalid date for row #{index}: #{regdate}"
-  end
-
-  # legislators = legislators_by_zipcode(zipcode)
-
-  # form_letter = erb_template.result(binding)
-  # save_thank_you_letter(id, form_letter)
+  hours[index] = get_hour(regdate)
+  days[index] = get_day(regdate)
 end
+puts "These are the hours in which most users signed up: #{calculate_peak_hours(hours)}"
+puts "These are the hours in which most users signed up: #{calculate_peak_days(days)}"
+
+# legislators = legislators_by_zipcode(zipcode)
+
+# form_letter = erb_template.result(binding)
+# save_thank_you_letter(id, form_letter)
 # contents = File.read('event_attendees.csv')
 # puts contents
 # lines = File.readlines('event_attendees.csv')
